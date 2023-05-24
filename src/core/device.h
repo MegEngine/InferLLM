f2e347c9d30eeb43b0c18242fbb456a28dc94418
SuperCB <1956699404@qq.com>
2023-05-24 23:36:04 +0800

    basic design of adding gpu

--- src/core/device.h
+++ src/core/device.h
@@ -7,44 +7,82 @@
 #include "utils.h"
 #include "thread_pool.h"
 
-namespace inferllm {
+
+#ifdef USE_GPU
+#include "../gpu_utils.cuh"
+#endif
+
+namespace inferllm
+{
+
+    class Tensor;
+
+    //! TODO: this task maybe as class to implement the CPU task and GPU task
+    using Task = std::function<void(void)>;
+
+    class Device
+    {
+    public:
+        Device(KernelType type, uint32_t nr_thread)
+        {
+            m_thread_pool = make_unique<ThreadPool>(nr_thread);
+            m_kernel = make_unique<Kernel>(type, m_thread_pool.get());
+        }
+        KernelType type() { return m_kernel->m_kernel_type; };
+
+        virtual ~Device();
+
+        void *allocate(size_t len);
+
+        void free_device(void *ptr);
+
+        //! move the source data from the device
+        void move_data_into(void *dst_data, void *src_data, size_t size,
+                            const Device &device) {}
+
+        Kernel *kernel() { return m_kernel.get(); }
+
+    private:
+        std::unique_ptr<Kernel> m_kernel;
+        std::unique_ptr<ThreadPool> m_thread_pool;
+        std::map<size_t, std::vector<void *>> m_free_memory;
+        std::map<void *, size_t> m_alloc_memory;
+
+        void *aligned_alloc(size_t size);
+        void aligned_free(void *ptr);
+    };
+
+  class CPUDevice : public Device{
 
 
+  };
 
-class Tensor;
+  class GPUDevice 
+  {
 
-//! TODO: this task maybe as class to implement the CPU task and GPU task
-using Task = std::function<void(void)>;
 
-class Device {
-public:
-    Device(KernelType type, uint32_t nr_thread) {
-        m_thread_pool = make_unique<ThreadPool>(nr_thread);
-        m_kernel = make_unique<Kernel>(type, m_thread_pool.get());
-    }
-    KernelType type() { return m_kernel->m_kernel_type; };
+    // Device(KernelType type, uint32_t nr_thread)
+    //     {
+    //     }
+        KernelType type() { return m_kernel->m_kernel_type; };
 
-    virtual ~Device();
+        virtual ~GPUDevice();
 
-    void* allocate(size_t len);
+        void *allocate(size_t len);
 
-    void free_device(void* ptr);
+        void free_device(void *ptr);
 
-    //! move the source data from the device
-    void move_data_into(void* dst_data, void* src_data, size_t size,
-                        const Device& device) {}
+        //! move the source data from the device
+        void move_data_into(void *dst_data, void *src_data, size_t size,
+                            const Device &device) {}
 
-    Kernel* kernel() { return m_kernel.get(); }
+        Kernel *kernel() { return m_kernel.get(); }
+    private:
+        std::unique_ptr<Kernel> m_kernel;
+        std::map<size_t, std::vector<void *>> m_free_memory;
+        std::map<void *, size_t> m_alloc_memory;
+        void *aligned_alloc(size_t size);
+        void aligned_free(void *ptr);
 
-private:
-    std::unique_ptr<Kernel> m_kernel;
-    std::unique_ptr<ThreadPool> m_thread_pool;
-    std::map<size_t, std::vector<void*>> m_free_memory;
-    std::map<void*, size_t> m_alloc_memory;
-
-    void* aligned_alloc(size_t size);
-    void aligned_free(void* ptr);
-};
-
-}  // namespace inferllm
-
+  };
+} // namespace inferllm
--- src/kern/kernel.h
+++ src/kern/kernel.h
@@ -10,31 +10,45 @@
 
 #include "kern/optimized/kernel_opt.h"
 
-namespace inferllm {
-class Kernel {
-public:
-    Kernel(KernelType kernel_type, ThreadPool* thread_pool)
-            : m_kernel_type(kernel_type), m_thread_pool(thread_pool) {
-    }
-
-    uint32_t nr_thread() const { return m_thread_pool->nr_threads(); }
-
-    //! compute
-    template <KernelID Id, typename... Args>
-    void operator()(Args... args) {
-        TaskSet task_set = opt::Comp<Id, Args...>::get_all_task(
-                std::forward<Args>(args)...);
-        //! parallel to execute tasks
-        for (auto& task : task_set) {
-            m_thread_pool->add_task(task.first, task.second);
+namespace inferllm
+{
+    class Kernel
+    {
+    public:
+        Kernel(KernelType kernel_type) : m_kernel_type(kernel_type)
+        {
         }
-    }
+        Kernel(KernelType kernel_type, ThreadPool *thread_pool)
+            : m_kernel_type(kernel_type), m_thread_pool(thread_pool)
+        {
+        }
 
-    template <KernelID Id, typename... Args>
-    size_t get_workspace(Args... args) {
-        return opt::Space<Id, Args...>::get(std::forward<Args>(args)...);
-    }
-    ThreadPool* m_thread_pool;
-    KernelType m_kernel_type;
-};
-}  // namespace inferllm
+        uint32_t nr_thread() const { return m_thread_pool->nr_threads(); }
+
+        //! compute
+        template <KernelID Id, typename... Args>
+        void operator()(Args... args)
+        {
+            TaskSet task_set = opt::Comp<Id, Args...>::get_all_task(
+                std::forward<Args>(args)...);
+            //! parallel to execute tasks
+            if (m_kernel_type == KernelType::GPU)
+            {
+            }
+            else
+            {
+                for (auto &task : task_set)
+                {
+                    m_thread_pool->add_task(task.first, task.second);
+                }
+            }
+        };
+        template <KernelID Id, typename... Args>
+        size_t get_workspace(Args... args)
+        {
+            return opt::Space<Id, Args...>::get(std::forward<Args>(args)...);
+        }
+        ThreadPool *m_thread_pool;
+        KernelType m_kernel_type;
+    }// namespace inferllm
+}
