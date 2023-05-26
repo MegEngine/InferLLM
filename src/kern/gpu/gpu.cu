8c423667ed5a79648a43d3e5b01c7e605eb8140b
SuperCB <1956699404@qq.com>
2023-05-26 13:01:42 +0800

    add more gpu operator

--- src/kern/gpu/gpu.cu
+++ src/kern/gpu/gpu.cu
@@ -8,15 +8,11 @@
 namespace inferllm {
 namespace gpu {
 
-
-
-
-__global void llm_softmax_compute_float_gpu(const float* src, float* dst,
-                                       uint32_t len_row, uint32_t col) {
-    const int row = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row < len_row) {
-        const float* psrc = src + row * col;
-        float* pdst = dst + row * col;
+__global__ void llm_softmax_compute_float_gpu(
+        const float* src, float* dst, uint32_t len_row, uint32_t col) {
+    CUDA_KERNEL_LOOP(index, len_row) {
+        const float* psrc = src + index * col;
+        float* pdst = dst + index * col;
 
         float max = -INFINITY;
         for (uint32_t i = 0; i < col; ++i) {
@@ -42,140 +38,169 @@ __global void llm_softmax_compute_float_gpu(const float* src, float* dst,
     }
 }
 
-void  llm_softmax_compute_float(const float* src, float* dst,
-                                  uint32_t len_row, uint32_t col) {
-    auto task = [=](const TaskId& id) {
-        for (uint32_t row = id.start; row < id.end; row++) {
-            const float* psrc = src + row * col;
-            float* pdst = dst + row * col;
+void llm_softmax_compute_float(
+        const float* src, float* dst, uint32_t len_row, uint32_t col) {
+    llm_softmax_compute_float_gpu<<<GET_BLOCKS(len_row), CUDA_NUM_THREADS>>>(
+            src, dst, len_row, col);
+}
 
-            float max = -INFINITY;
-            for (uint32_t i = 0; i < col; ++i) {
-                max = std::max(max, psrc[i]);
-            }
+__global__ llm_norm_compute_float_gpu(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd) {
+    CUDA_KERNEL_LOOP(i, seq_len) {
+        const float eps = 1e-5f;
+        const float* row = src + i * embd;
+        float* out = dst + i * embd;
 
-            float sum = 0.0f;
-
-            for (uint32_t i = 0; i < col; i++) {
-                if (psrc[i] == -INFINITY) {
-                    pdst[i] = 0.0f;
-                } else {
-                    float val = exp(psrc[i] - max);
-                    sum += val;
-                    pdst[i] = val;
-                }
-            }
-
-            sum = 1.0 / sum;
-            for (uint32_t j = 0; j < col; j++) {
-                pdst[j] = pdst[j] * sum;
-            }
+        float mean = 0.0;
+        for (uint32_t j = 0; j < embd; j++) {
+            mean += row[j] * row[j];
         }
-    };
-}
+        mean /= embd;
 
+        const float scale = 1.0 / sqrt(mean + eps);
 
-
-TaskSet llm_embedding_get_int4_float(
-        const void* weights, const uint32_t* index, float* dst, uint32_t len_seq,
-        uint32_t embd) {
-    auto task = [=](const TaskId& id) {
-        for (uint32_t i = id.start; i < id.end; ++i) {
-            const int row = index[i];
-            const int weight_stride =
-                    embd * dtype_in_byte(DType::Int4) / dtype_block_size(DType::Int4);
-            dequantize_row_q4_0_reference(
-                    (static_cast<const char*>(weights) + row * weight_stride),
-                    dst + i * embd, embd);
+        for (uint32_t j = 0; j < embd; j++) {
+            out[j] = row[j] * scale;
         }
-    };
-    return TaskSet{{task, len_seq}};
+    }
 }
 
-__global__ void llm_embedding_get_float_float_gpu(
-        const float* weights, const uint32_t* index, float* dst, uint32_t len_seq,
-        uint32_t embd) {
-    const int row = index[i];
-    const int weight_stride = embd;
-    memcpy(dst + i * embd, weights + row * weight_stride, embd * sizeof(float));
-}
-__global void llm_embefing
-
-
-
-
-void llm_embedding_get_float_float(
-        const float* weights, const uint32_t* index, float* dst, uint32_t len_seq,
-        uint32_t embd) {
-    auto task = [=](const TaskId& id) {
-        for (uint32_t i = id.start; i < id.end; ++i) {
-            const int row = index[i];
-            const int weight_stride = embd;
-            memcpy(dst + i * embd, weights + row * weight_stride, embd * sizeof(float));
-        }
-    };
-    return TaskSet{{task, len_seq}};
+void llm_norm_compute_float(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd) {
+    llm_norm_compute_float_gpu<<<GET_BLOCKS(len_row), CUDA_NUM_THREADS>>>(
+            src, dst, seq_len, embd);
 }
 
-
-
-__global__ void llm_elem
-
-
-
-// TaskSet llm_elemwise_compute_float(
-//         InData<float> srcs, float* dst, size_t len, ElemMode mode) {
-//     MultiThreadingTask task;
-//     switch (mode) {
-//         case ElemMode::Add: {
-//             task = [=](const TaskId& id) {
-//                 const float* src0 = srcs[0];
-//                 const float* src1 = srcs[1];
-//                 for (size_t i = id.start; i < id.end; i++) {
-//                     dst[i] = src0[i] + src1[i];
-//                 }
-//             };
-//             break;
+// TaskSet llm_embedding_get_int4_float(
+//         const void* weights, const uint32_t* index, float* dst, uint32_t len_seq,
+//         uint32_t embd) {
+//     auto task = [=](const TaskId& id) {
+//         for (uint32_t i = id.start; i < id.end; ++i) {
+//             const int row = index[i];
+//             const int weight_stride =
+//                     embd * dtype_in_byte(DType::Int4) / dtype_block_size(DType::Int4);
+//             dequantize_row_q4_0_reference(
+//                     (static_cast<const char*>(weights) + row * weight_stride),
+//                     dst + i * embd, embd);
 //         }
-//         case ElemMode::Mul: {
-//             task = [=](const TaskId& id) {
-//                 const float* src0 = srcs[0];
-//                 const float* src1 = srcs[1];
-//                 for (size_t i = id.start; i < id.end; i++) {
-//                     dst[i] = src0[i] * src1[i];
-//                 }
-//             };
-//             break;
-//         }
-//         case ElemMode::Silu: {
-//             task = [=](const TaskId& id) {
-//                 const float* src0 = srcs[0];
-//                 for (size_t i = id.start; i < id.end; i++) {
-//                     float src = src0[i];
-//                     dst[i] = src / (1.0 + exp(-src));
-//                 }
-//                 return;
-//             };
-//             break;
-//         }
-//         case ElemMode::Gelu: {
-//             task = [=](const TaskId& id) {
-//                 const float* src0 = srcs[0];
-//                 for (size_t i = id.start; i < id.end; i++) {
-//                     float src = src0[i];
-//                     dst[i] = 0.5 * src *
-//                              (1 +
-//                               tanh(sqrt(2.0 / PI) * (src + PGELU * src * src * src)));
-//                 }
-//                 return;
-//             };
-//             break;
-//         }
-//         default:
-//             INFER_ASSERT(0, "Not supported.");
-//     }
-//     return TaskSet{{task, len}};
+//     };
+//     return TaskSet{{task, len_seq}};
 // }
 
+// __global__ void llm_embedding_get_float_float_gpu(
+//         const float* weights, const uint32_t* index, float* dst, uint32_t len_seq,
+//         uint32_t embd) {
+//     // const int row = index[i];
+//     // const int weight_stride = embd;
+
+//     // for ()
+
+//     // memcpy(dst + i * embd, weights + row * weight_stride, embd * sizeof(float));
+// }
+
+// //
+// void llm_embedding_get_float_float(
+//         const float* weights, const uint32_t* index, float* dst, uint32_t len_seq,
+//         uint32_t embd) {
+//     auto task = [=](const TaskId& id) {
+//         for (uint32_t i = id.start; i < id.end; ++i) {
+//             const int row = index[i];
+//             const int weight_stride = embd;
+//             memcpy(dst + i * embd, weights + row * weight_stride, embd *
+//             sizeof(float));
+//         }
+//     };
+//     return TaskSet{{task, len_seq}};
+// }
+
+struct SiluFunctor {
+    __device__ float operator()(uint32_t i, float* input) const {
+        float src = input[i];
+        return src / (1.0 + exp(-src));
+    }
+};
+
+struct GeluFunctor {
+    __device__ float operator()(uint32_t i, float* input) const {
+        float src = input[i];
+        return 0.5 * src * (1 + tanh(sqrt(2.0 / PI) * (src + PGELU * src * src * src)));
+    }
+}
+
+struct AddFunctor {
+    __device__ float operator()(uint32_t i, float* input1, float* input2) const {
+        return input1[i] + input2[i];
+    }
+};
+
+struct MultiplyFunctor {
+    __device__ float operator()(uint32_t i, float* input1, float* input2) const {
+        return input1[i] + input2[i];
+    }
+};
+
+void llm_elemwise_compute_float(
+        InData<float> srcs, float* dst, size_t len, ElemMode mode) {
+    MultiThreadingTask task;
+    switch (mode) {
+        case ElemMode::Add: {
+            const float* src0 = srcs[0];
+            const float* src1 = srcs[1];
+            LaunchKernel(AddFunctor(), N, dst, src0, src1);
+            break;
+        }
+        case ElemMode::Mul: {
+            const float* src0 = srcs[0];
+            LaunchKernel(SiluFunctor(), N, dst, src0);
+            break;
+        }
+        case ElemMode::Silu: {
+            const float* src0 = srcs[0];
+            LaunchKernel(SiluFunctor(), N, dst, src0);
+            break;
+        }
+        case ElemMode::Gelu: {
+            {
+                const float* src0 = srcs[0];
+                LaunchKernel(GeluFunctor(), N, dst, src0);
+                break;
+            }
+            default:
+                INFER_ASSERT(0, "Not supported.");
+        }
+            return TaskSet{{task, len}};
+    }
+
 }  // namespace gpu
-}  // namespace inferllm
+
+__global__ llm_rms_norm_compute_float_gpu(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd) {
+    CUDA_KERNEL_LOOP(i, seq_len) {
+        for (uint32_t i = id.start; i < id.end; i++) {
+            const float* row = src + i * embd;
+            float* out = dst + i * embd;
+
+            float mean = 0.0;
+            for (uint32_t j = 0; j < embd; j++) {
+                mean += row[j] * row[j];
+            }
+            mean /= embd;
+
+            const float scale = 1.0 / sqrt(mean + eps);
+
+            for (uint32_t j = 0; j < embd; j++) {
+                out[j] = row[j] * scale;
+            }
+        }
+    }
+}
+
+
+
+void llm_rms_norm_compute_float(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd) {
+    llm_rms_norm_compute_float_gpu<<<GET_BLOCKS(len_row), CUDA_NUM_THREADS>>>(
+            src, dst, seq_len, embd);
+}
+
+}  // namespace gpu
--- src/kern/gpu/gpu.cuh
+++ src/kern/gpu/gpu.cuh
@@ -1,49 +1,53 @@
 #pragma once
 #include <cuda.h>
+#include <cuda_fp16.h>
 #include <cuda_runtime.h>
 #include <time.h>
 #include <algorithm>
 #include <iostream>
-#include <cuda_fp16.h>
 using namespace std;
 
-#define N  1024
-// elementwise implementation copyed from https://github.com/Oneflow-Inc/oneflow/blob/master/oneflow/core/cuda/elementwise.cuh
+// elementwise implementation copyed from
+// https://github.com/Oneflow-Inc/oneflow/blob/master/oneflow/core/cuda/elementwise.cuh
 namespace inferllm {
 namespace gpu {
 
 constexpr int kBlockSize = 256;
 constexpr int kNumWaves = 32;
 
-inline cudaError_t GetNumBlocks(int64_t n, int *num_blocks)
-{
+inline cudaError_t GetNumBlocks(int64_t n, int* num_blocks) {
     int dev;
     {
         cudaError_t err = cudaGetDevice(&dev);
-        if (err != cudaSuccess)
-        {
+        if (err != cudaSuccess) {
             return err;
         }
     }
     int sm_count;
     {
-        cudaError_t err = cudaDeviceGetAttribute(&sm_count, cudaDevAttrMultiProcessorCount, dev);
-        if (err != cudaSuccess)
-        {
+        cudaError_t err =
+                cudaDeviceGetAttribute(&sm_count, cudaDevAttrMultiProcessorCount, dev);
+        if (err != cudaSuccess) {
             return err;
         }
     }
     int tpm;
     {
-        cudaError_t err = cudaDeviceGetAttribute(&tpm, cudaDevAttrMaxThreadsPerMultiProcessor, dev);
-        if (err != cudaSuccess)
-        {
+        cudaError_t err = cudaDeviceGetAttribute(
+                &tpm, cudaDevAttrMaxThreadsPerMultiProcessor, dev);
+        if (err != cudaSuccess) {
             return err;
         }
     }
-    *num_blocks = std::max<int>(1, std::min<int64_t>((n + kBlockSize - 1) / kBlockSize,
-                                                     sm_count * tpm / kBlockSize * kNumWaves));
+    *num_blocks = std::max<int>(
+            1, std::min<int64_t>(
+                       (n + kBlockSize - 1) / kBlockSize,
+                       sm_count * tpm / kBlockSize * kNumWaves));
     return cudaSuccess;
+}
+
+inline int GET_BLOCKS(const int N) {
+    return (N + CUDA_NUM_THREADS - 1) / CUDA_NUM_THREADS;
 }
 
 constexpr int kMaxPackBytes = 128 / 8;
@@ -51,137 +55,103 @@ constexpr int kMaxPackSize = 8;
 
 template <typename Function, typename... Args>
 __global__ void __launch_bounds__(kBlockSize)
-    ApplyGeneric(Function functor, int64_t n, float *ret, Args... args)
-{
-
+        ApplyFunction(Function functor, int64_t n, float* ret, Args... args) {
     const int global_tid = blockIdx.x * kBlockSize + threadIdx.x;
-    for (int64_t i = global_tid; i < n; i += blockDim.x * gridDim.x)
-    {
+    for (int64_t i = global_tid; i < n; i += blockDim.x * gridDim.x) {
         ret[i] = functor(i, args...);
     }
 }
 
 template <typename Function, typename... Args>
-cudaError_t LaunchKernel(Function fun, int64_t n, float *ret, Args... args)
-{
+cudaError_t LaunchKernel(Function fun, int64_t n, float* ret, Args... args) {
     int num_blocks;
     {
-        cudaError_t err = GetNumBlocks(n,&num_blocks);
-        if (err != cudaSuccess)
-        {
+        cudaError_t err = GetNumBlocks(n, &num_blocks);
+        if (err != cudaSuccess) {
             return err;
         }
     }
-    ApplyGeneric<<<num_blocks, kBlockSize>>>(fun, n, ret, args...);
+    ApplyFunction<<<num_blocks, kBlockSize>>>(fun, n, ret, args...);
     return cudaPeekAtLastError();
 }
-
-struct MultiplyFunctor
-{
-    __device__ float operator()(uint32_t i, float *input1, float *input2) const
-    {
-        return input1[i] + input2[i];
-    }
-};
-
-
-
-
-
-
-
-
-
-
 
 // CUDA: library error reporting.
 const char* cublasGetErrorString(cublasStatus_t error);
 const char* curandGetErrorString(curandStatus_t error);
 
 // CUDA: use 512 threads per block
-const int KUIPER_CUDA_NUM_THREADS = 512;
+const int CUDA_NUM_THREADS = 512;
 
 // CUDA: number of blocks for threads.
-inline int GET_BLOCKS(const int N) {
-  return (N + KUIPER_CUDA_NUM_THREADS - 1) / KUIPER_CUDA_NUM_THREADS;
-}
 
+void llm_embedding_get_int4_float(
+        const void* weights, const uint32_t* index, float* dst, uint32_t len_seq,
+        uint32_t embd);
+void llm_embedding_get_float_float(
+        const float* weights, const uint32_t* index, float* dst, uint32_t len_seq,
+        uint32_t embd);
 
-void  llm_embedding_get_int4_float(const void* weights, const uint32_t* index,
-                                     float* dst, uint32_t len_seq,
-                                     uint32_t embd);
-void llm_embedding_get_float_float(const float* weights,
-                                      const uint32_t* index, float* dst,
-                                      uint32_t len_seq, uint32_t embd);
+void llm_elemwise_compute_float(
+        InData<float> srcs, float* dst, size_t len, ElemMode mode);
 
-void llm_elemwise_compute_float(InData<float> srcs, float* dst, size_t len,
-                                   ElemMode mode);
-
-void llm_elemwise_compute_float_scale(float* src, float* dst, size_t len,
-                                   float scale);
+void llm_elemwise_compute_float_scale(float* src, float* dst, size_t len, float scale);
 
 void llm_elemwise_broadcast_dim0_src1_compute_float(
-        const float* src0, const float* src1, float* dst, uint32_t len0,
-        uint32_t len1, ElemMode mode);
+        const float* src0, const float* src1, float* dst, uint32_t len0, uint32_t len1,
+        ElemMode mode);
 
-void llm_norm_compute_float(const float* src, float* dst, uint32_t seq_len,
-                               uint32_t embd);
+void llm_norm_compute_float(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd);
 
-void llm_rms_norm_compute_float(const float* src, float* dst,
-                                   uint32_t seq_len, uint32_t embd);
+void llm_rms_norm_compute_float(
+        const float* src, float* dst, uint32_t seq_len, uint32_t embd);
 
-void llm_softmax_compute_float(const float* src, float* dst,
-                                  uint32_t len_row, uint32_t col);
+void llm_softmax_compute_float(
+        const float* src, float* dst, uint32_t len_row, uint32_t col);
 
 // compute the softmax of the last dim of src, and store the result in dst
-void llm_matmul_compute_int4_float(float* dst, const void* src0,
-                                      const float* bias, const float* src1,
-                                      uint32_t M, uint32_t N, uint32_t K,
-                                      void* workspace, uint32_t size);
-void llm_matmul_compute_float_float(float* dst, const float* src0,
-                                      const float* bias, const float* src1,
-                                      uint32_t M, uint32_t N, uint32_t K,
-                                      void* workspace, uint32_t size);
+void llm_matmul_compute_int4_float(
+        float* dst, const void* src0, const float* bias, const float* src1, uint32_t M,
+        uint32_t N, uint32_t K, void* workspace, uint32_t size);
+void llm_matmul_compute_float_float(
+        float* dst, const float* src0, const float* bias, const float* src1, uint32_t M,
+        uint32_t N, uint32_t K, void* workspace, uint32_t size);
 
-size_t llm_matmul_get_workspace_float(uint32_t nr_thread, uint32_t M,
-                                      uint32_t N, uint32_t K);
+size_t llm_matmul_get_workspace_float(
+        uint32_t nr_thread, uint32_t M, uint32_t N, uint32_t K);
 
-size_t llm_matmul_get_workspace_float_float(uint32_t nr_thread, uint32_t M,
-                                            uint32_t N, uint32_t K);
+size_t llm_matmul_get_workspace_float_float(
+        uint32_t nr_thread, uint32_t M, uint32_t N, uint32_t K);
 
-void llm_rope_compute_float(float* dst, const float* src0, uint32_t n_past,
-                               uint32_t n_rot, RotMode m, uint32_t N,
-                               uint32_t head, uint32_t embd);
+void llm_rope_compute_float(
+        float* dst, const float* src0, uint32_t n_past, uint32_t n_rot, RotMode m,
+        uint32_t N, uint32_t head, uint32_t embd);
 
-void llm_glm_rope_compute_float(float* dst, const float* src0,
-                                   uint32_t n_past, uint32_t gmask_positon,
-                                   uint32_t seqlen, uint32_t head,
-                                   uint32_t embd);
+void llm_glm_rope_compute_float(
+        float* dst, const float* src0, uint32_t n_past, uint32_t gmask_positon,
+        uint32_t seqlen, uint32_t head, uint32_t embd);
 
-void llm_diag_mask_inf_float(float* dst, const float* src0, uint32_t n_past,
-                                uint32_t N, uint32_t head);
+void llm_diag_mask_inf_float(
+        float* dst, const float* src0, uint32_t n_past, uint32_t N, uint32_t head);
 
-void llm_glm_gmask_inf_float(float* dst, uint32_t n_past, uint32_t seqlen,
-                                uint32_t head);
+void llm_glm_gmask_inf_float(
+        float* dst, uint32_t n_past, uint32_t seqlen, uint32_t head);
 
-void llm_scale_diag_mask_inf_float(float* dst, const float* src0,
-                                      float scale, uint32_t n_past,
-                                      uint32_t seqlen, uint32_t head);
+void llm_scale_diag_mask_inf_float(
+        float* dst, const float* src0, float scale, uint32_t n_past, uint32_t seqlen,
+        uint32_t head);
 
-void llm_permute_compute_float(float* dst, const float* src0, uint32_t dim0,
-                                  uint32_t dim1, uint32_t dim2,
-                                  std::vector<uint32_t> param);
+void llm_permute_compute_float(
+        float* dst, const float* src0, uint32_t dim0, uint32_t dim1, uint32_t dim2,
+        std::vector<uint32_t> param);
 
-void llm_matmul_compute_with_head_stride_float(float* dst, const float* srck,
-                                                  const float* srcq,
-                                                  uint32_t seqlen,
-                                                  uint32_t embd, uint32_t head,
-                                                  uint32_t nr_past);
+void llm_matmul_compute_with_head_stride_float(
+        float* dst, const float* srck, const float* srcq, uint32_t seqlen,
+        uint32_t embd, uint32_t head, uint32_t nr_past);
 
-void llm_head_batched_matmul_compute_float(float* dst, const float* v,
-                                              const float* qk, uint32_t seqlen,
-                                              uint32_t embd, uint32_t head,
-                                              uint32_t nr_past);
+void llm_head_batched_matmul_compute_float(
+        float* dst, const float* v, const float* qk, uint32_t seqlen, uint32_t embd,
+        uint32_t head, uint32_t nr_past);
 template <KernelID Id, typename... Args>
 struct Comp {
     static void get_all_task(Args... args);
@@ -192,7 +162,7 @@ struct Space {
     static size_t get(Args... args);
 };
 
-}  // namespace naive
+}  // namespace gpu
 
 }  // namespace inferllm
 #ifdef PartialImplementKernel
@@ -202,20 +172,18 @@ struct Space {
 #undef PartialImplementSpace
 #endif
 
-#define PartialImplementKernel(kernel_id, fun)       \
-    template <typename... Args>                      \
-    struct Comp<KernelID::kernel_id, Args...> {      \
-        static void get_all_task(Args... args) {  \
-            return fun(std::forward<Args>(args)...); \
-        }                                            \
+#define PartialImplementKernel(kernel_id, fun)                                         \
+    template <typename... Args>                                                        \
+    struct Comp<KernelID::kernel_id, Args...> {                                        \
+        static void get_all_task(Args... args) {                                       \
+            return fun(std::forward<Args>(args)...);                                   \
+        }                                                                              \
     };
 
-#define PartialImplementSpace(kernel_id, fun)        \
-    template <typename... Args>                      \
-    struct Space<KernelID::kernel_id, Args...> {     \
-        static size_t get(Args... args) {            \
-            return fun(std::forward<Args>(args)...); \
-        }                                            \
+#define PartialImplementSpace(kernel_id, fun)                                          \
+    template <typename... Args>                                                        \
+    struct Space<KernelID::kernel_id, Args...> {                                       \
+        static size_t get(Args... args) { return fun(std::forward<Args>(args)...); }   \
     };
 
 namespace inferllm {
@@ -248,5 +216,5 @@ PartialImplementKernel(ElemwiseFloatScale, llm_elemwise_compute_float_scale);
 #undef PartialImplementKernel
 #undef PartialImplementSpace
 
-}  // namespace naive
+}  // namespace gpu
 }  // namespace inferllm
